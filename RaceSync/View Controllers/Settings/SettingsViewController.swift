3dc9445b9da7f93624d4d2cc13752fbed2bfc969
dzenbot <ignacio.romeroz@gmail.com>
2020-01-18 13:59:50 -0800

    Preparing to work on the Settings view

---
+++ RaceSync/View Controllers/Settings/SettingsViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SettingsViewController.swift
+//  RaceSync
+//
+//  Created by Ignacio Romero Zurbuchen on 2020-01-18.
+//  Copyright © 2020 MultiGP Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    // MARK: - Private Variables
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        return tableView
+    }()
+
+    fileprivate enum Constants {
+        static let padding: CGFloat = UniversalConstants.padding
+        static let cellHeight: CGFloat = UniversalConstants.cellHeight
+    }
+
+    // MARK: - Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupLayout()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    // MARK: - Layout
+
+    func setupLayout() {
+
+    }
+
+}
+
+extension SettingsViewController: UITableViewDelegate {
+
+}
+
+extension SettingsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constants.cellHeight
+    }
+}
+
