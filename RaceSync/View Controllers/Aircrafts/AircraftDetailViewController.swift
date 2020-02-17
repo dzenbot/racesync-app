423f8ffa8b8ecfa6bcf60d6bbd465f74017c5f88
dzenbot <ignacio.romeroz@gmail.com>
2020-02-17 09:54:10 -0800

    Adding empty class for now

---
+++ RaceSync/View Controllers/Aircrafts/AircraftDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AircraftDetailViewController.swift
+//  RaceSync
+//
+//  Created by Ignacio Romero Zurbuchen on 2020-02-17.
+//  Copyright © 2020 MultiGP Inc. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import RaceSyncAPI
+
+class AircraftDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
