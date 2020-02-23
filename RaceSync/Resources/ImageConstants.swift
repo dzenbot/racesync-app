de033fafaf22bcb7c2fda8af42e43be65badae74
dzenbot <ignacio.romeroz@gmail.com>
2020-02-23 08:13:54 -0800

    Including placeholder constants, for cleaner code

---
+++ RaceSync/Resources/ImageConstants.swift
@@ -0,0 +1,24 @@
+//
+//  ImageConstants.swift
+//  RaceSync
+//
+//  Created by Ignacio Romero Zurbuchen on 2020-02-23.
+//  Copyright © 2020 MultiGP Inc. All rights reserved.
+//
+
+import UIKit
+
+public class Placeholder {
+    public static let Small = UIImage(named: "placeholder_small")
+    public static let Medium = UIImage(named: "placeholder_medium")
+    public static let Large = UIImage(named: "placeholder_large")
+
+    public static let ProfileAvatar = UIImage(named: "placeholder_profile_avatar")
+    public static let ProfileAircraft = UIImage(named: "placeholder_profile_aircraft")
+    public static let ProfileBackground = UIImage(named: "placeholder_profile_background")
+
+    public static let LargeAircraft = UIImage(named: "placeholder_large_aircraft")
+    public static let LargeAircraftCreate = UIImage(named: "placeholder_large_aircraft_create")
+
+    public static let ShimmerList = UIImage(named: "placeholder_shimmer_list")
+}
