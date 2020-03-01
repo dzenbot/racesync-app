a859643e9230bf50869478b01e606e8345bc476c
dzenbot <ignacio.romeroz@gmail.com>
2020-02-29 16:09:43 -0800

    Handle crash catcher error too

--- RaceSync/Tools/CrashCatcher.swift
+++ RaceSync/Tools/CrashCatcher.swift
@@ -13,7 +13,12 @@ import Sentry
 class CrashCatcher {
 
     static func configure() {
-        Client.shared = try? Client(dsn: "https://4dbd7fdde60b4c828846d94fecc814c1@sentry.io/3036524")
-        try? Client.shared?.startCrashHandler()
+        // Create a Sentry client and start crash handler
+        do {
+            Client.shared = try Client(dsn: "https://4dbd7fdde60b4c828846d94fecc814c1@sentry.io/3036524")
+            try Client.shared?.startCrashHandler()
+        } catch let error {
+            print("\(error)")
+        }
     }
 }
