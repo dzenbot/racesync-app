2413b6fd203ac4a7627329368674dc1fbfb577de
dzenbot <ignacio.romeroz@gmail.com>
2020-12-25 14:08:00 -0800

    Adding WatchOS App target + RaceSync Icon images

---
+++ RaceSyncWatch Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  RaceSyncWatch Extension
+//
+//  Created by Ignacio Romero Zurbuchen on 2020-12-25.
+//  Copyright © 2020 MultiGP Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
