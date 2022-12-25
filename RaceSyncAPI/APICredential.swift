813b46dce944ba67e1c8b7afe74f47d002c278c3
dzenbot <ignacio.romeroz@gmail.com>
2022-12-25 11:13:03 -0800

    Not versioning the API key anymore

--- RaceSyncAPI/APICredential.swift
+++ RaceSyncAPI/APICredential.swift
@@ -10,28 +10,27 @@ import Foundation
 
 public class APICredential {
     public let apiKey: String
-    public let email: String?
-    public let password: String?
+    public let email: String
+    public let password: String
 
     init() {
-        apiKey = "3WlfklZkaSO7p8Y3qwxebeSEMllyLVyPST4cf4xqWmxmuwqU2Y9dc2SYnex9a5Y2Z3ff8MF48drCJRxLPHZ2KS186yihEgjDkyTslyxtLY6uQEgFlgI68JefiwwWNQA7"
+        let bundle = Bundle(for: APICredential.self)
 
-        // Development tool for auto-completing the login screen
-        #if DEBUG
-            let bundle = Bundle(for: APICredential.self)
+        // TODO: Throw and print error
+        if let path = bundle.path(forResource: "Credentials", ofType: "plist"),
+            let dict = NSDictionary(contentsOfFile: path) {
 
-            // TODO: Throw and print error
-            if let path = bundle.path(forResource: "Credentials", ofType: "plist"),
-                let dict = NSDictionary(contentsOfFile: path) {
-                email = dict["EMAIL"] as? String
-                password = dict["PASSWORD"] as? String
-            } else {
-                email = nil
-                password = nil
-            }
-        #else
-            email = nil
-            password = nil
-        #endif
+            // Used during development for auto-completing the login screen
+            #if DEBUG
+            email = dict["EMAIL"] as? String ?? ""
+            password = dict["PASSWORD"] as? String ?? ""
+            #endif
+
+            apiKey = dict["API_KEY"] as? String ?? ""
+        } else {
+            email = ""
+            password = ""
+            apiKey = ""
+        }
     }
 }
