ac903e0bb05236f41c5c1063c91a12db4b174b1e
dzenbot <ignacio.romeroz@gmail.com>
2022-12-27 11:07:01 -0800

    Adding new race enums

--- RaceSyncAPI/Constants/RaceConstants.swift
+++ RaceSyncAPI/Constants/RaceConstants.swift
@@ -32,3 +32,44 @@ public enum RaceOfficialStatus: String {
     case requested = "1"
     case approved = "2"
 }
+
+//SCORING_FORMATS = array(0=>'Aggregate Laps', 1=>'Fastest Lap', 6=>'Fastest 2 Consecutive Laps', 2=>'Fastest 3 Consecutive Laps')
+public enum ScoringFormats: String, EnumTitle {
+    case aggregate = "0"
+    case fastestLap = "1"
+    case fastest2Laps = "6"
+    case fastest3Laps = "2"
+
+    public var title: String {
+        switch self {
+        case .aggregate:        return "Aggregate Laps"
+        case .fastestLap:       return "Fastest Lap"
+        case .fastest2Laps:     return "Fastest 2 Consecutive Laps"
+        case .fastest3Laps:     return "Fastest 3 Consecutive Laps"
+        }
+    }
+}
+
+//RACE_CLASS = array(0 =>'Open', 1 =>'Tiny Whoop', 2 =>'Micro (Tiny Trainer)', 3 =>'Freedom Spec', 4 =>'Street League', 5 =>'Mega', 6 =>'Velocidrone')
+public enum RaceClass: String, EnumTitle {
+    case open = "0"
+    case whoop = "1"
+    case micro = "2"
+    case freedom = "3"
+    case street = "4"
+    case mega = "5"
+    case velo = "6"
+
+    public var title: String {
+        switch self {
+        case .open:         return "Open"
+        case .whoop:        return "Tiny Whoop"
+        case .micro:        return "Micro (Tiny Trainer)"
+        case .freedom:      return "Freedom Spec"
+        case .street:       return "Street League"
+        case .mega:         return "Mega"
+        case .velo:         return "Velocidrone"
+        }
+    }
+}
+
