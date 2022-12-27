3c7c11ea2b040080db90da75c60b1263f7ca3620
dzenbot <ignacio.romeroz@gmail.com>
2022-12-26 20:01:54 -0800

    Implementing Season and Course API methods

--- RaceSyncAPI/Network/CourseApi.swift
+++ RaceSyncAPI/Network/CourseApi.swift
@@ -8,16 +8,75 @@
 
 import Foundation
 import Alamofire
-import AlamofireObjectMapper
-import SwiftyJSON
 
 // MARK: - Interface
 public protocol CourseApiInterface {
 
+    /**
+     */
+    func getCourse(with courseId: ObjectId, _ completion: @escaping ObjectCompletionBlock<Course>)
+
+    /**
+     */
+    func searchCourse(with name: String, _ completion: @escaping ObjectCompletionBlock<Course>)
+
+    /**
+     */
+    func getCourses(forChapter chapterId: ObjectId, currentPage: Int, pageSize: Int, _ completion: @escaping ObjectCompletionBlock<[Course]>)
+
+    /**
+     */
+    func createCourse(forChapter chapterId: ObjectId, with parameters: Parameters, _ completion: @escaping ObjectCompletionBlock<Course>)
+
+    /**
+     */
+    func deleteCourse(with courseId: ObjectId, _ completion: @escaping StatusCompletionBlock)
 }
 
 public class CourseApi: CourseApiInterface {
 
+    public init() {}
+    fileprivate let repositoryAdapter = RepositoryAdapter()
+
+    public func getCourse(with courseId: ObjectId, _ completion: @escaping ObjectCompletionBlock<Course>) {
+
+        let endpoint = EndPoint.courseSearch
+        let params: Parameters = [ParameterKey.id: courseId]
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Course.self, completion)
+    }
+
+    public func searchCourse(with name: String, _ completion: @escaping ObjectCompletionBlock<Course>) {
+
+        let endpoint = EndPoint.courseSearch
+        let params: Parameters = [ParameterKey.name: name]
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Course.self, completion)
+    }
+
+    public func getCourses(forChapter chapterId: ObjectId, currentPage: Int = 0, pageSize: Int = StandardPageSize, _ completion: @escaping ObjectCompletionBlock<[Course]>) {
+
+        let endpoint = EndPoint.courseList
+        let params: Parameters = [ParameterKey.chapterId: chapterId]
+
+        repositoryAdapter.getObjects(endpoint, parameters: params, currentPage: currentPage, pageSize: pageSize, type: Course.self, completion)
+    }
+
+    public func createCourse(forChapter chapterId: ObjectId, with parameters: Parameters, _ completion: @escaping ObjectCompletionBlock<Course>) {
+
+        let endpoint = EndPoint.courseCreate
+        let params: Parameters = [ParameterKey.chapterId: chapterId]
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Course.self, completion)
+    }
+
+    public func deleteCourse(with courseId: ObjectId, _ completion: @escaping StatusCompletionBlock) {
+
+        let endpoint = EndPoint.courseDelete
+        let params: Parameters = [ParameterKey.id: courseId]
+
+        repositoryAdapter.performAction(endpoint, parameters: params, completion: completion)
+    }
 }
 
 fileprivate extension CourseApi {
--- RaceSyncAPI/Network/SeasonApi.swift
+++ RaceSyncAPI/Network/SeasonApi.swift
@@ -8,16 +8,76 @@
 
 import Foundation
 import Alamofire
-import AlamofireObjectMapper
-import SwiftyJSON
 
 // MARK: - Interface
 public protocol SeasonApiInterface {
 
+    /**
+     */
+    func getSeason(with seasonId: ObjectId, _ completion: @escaping ObjectCompletionBlock<Season>)
+
+    /**
+     */
+    func searchSeason(with name: String, _ completion: @escaping ObjectCompletionBlock<Season>)
+
+    /**
+     */
+    func getSeasons(forChapter chapterId: ObjectId, currentPage: Int, pageSize: Int, _ completion: @escaping ObjectCompletionBlock<[Season]>)
+
+    /**
+     */
+    func createSeason(forChapter chapterId: ObjectId, with parameters: Parameters, _ completion: @escaping ObjectCompletionBlock<Season>)
+
+    /**
+     */
+    func deleteSeason(with seasonId: ObjectId, _ completion: @escaping StatusCompletionBlock)
 }
 
 public class SeasonApi: SeasonApiInterface {
-    
+
+    public init() {}
+    fileprivate let repositoryAdapter = RepositoryAdapter()
+
+    public func getSeason(with seasonId: ObjectId, _ completion: @escaping ObjectCompletionBlock<Season>) {
+
+        let endpoint = EndPoint.seasonSearch
+        let params: Parameters = [ParameterKey.id: seasonId]
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Season.self, completion)
+    }
+
+    public func searchSeason(with name: String, _ completion: @escaping ObjectCompletionBlock<Season>) {
+
+        let endpoint = EndPoint.seasonSearch
+        let params: Parameters = [ParameterKey.name: name]
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Season.self, completion)
+    }
+
+    public func getSeasons(forChapter chapterId: ObjectId, currentPage: Int = 0, pageSize: Int = StandardPageSize, _ completion: @escaping ObjectCompletionBlock<[Season]>) {
+
+        let endpoint = EndPoint.seasonList
+        let params: Parameters = [ParameterKey.chapterId: chapterId]
+
+        repositoryAdapter.getObjects(endpoint, parameters: params, currentPage: currentPage, pageSize: pageSize, type: Season.self, completion)
+    }
+
+    public func createSeason(forChapter chapterId: ObjectId, with parameters: Parameters, _ completion: @escaping ObjectCompletionBlock<Season>) {
+
+        let endpoint = EndPoint.seasonCreate
+        var params: Parameters = parameters
+        params[ParameterKey.chapterId] = chapterId
+
+        repositoryAdapter.getObject(endpoint, parameters: params, type: Season.self, completion)
+    }
+
+    public func deleteSeason(with seasonId: ObjectId, _ completion: @escaping StatusCompletionBlock) {
+
+        let endpoint = EndPoint.seasonDelete
+        let params: Parameters = [ParameterKey.id: seasonId]
+
+        repositoryAdapter.performAction(endpoint, parameters: params, completion: completion)
+    }
 }
 
 fileprivate extension SeasonApi {
